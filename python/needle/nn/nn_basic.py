be9aaad2d6dadc6551df29f70ce3767583b37efc
Yifei Lu <yifeil0725@gmail.com>
2026-07-23 00:07:06 +0800

    implemented SoftmaxLoss

--- python/needle/nn/nn_basic.py
+++ python/needle/nn/nn_basic.py
@@ -128,7 +128,11 @@ class Sequential(Module):
 class SoftmaxLoss(Module):
     def forward(self, logits: Tensor, y: Tensor) -> Tensor:
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        N = logits.shape[0]
+        log_probs = ops.logsoftmax(logits)
+        one_hot = init.one_hot(logits.shape[1], y, device=logits.device, dtype=logits.dtype)
+        loss = -ops.summation(log_probs * one_hot) / N
+        return loss
         ### END YOUR SOLUTION
 
 
