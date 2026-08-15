05ed9946d6fc4a94f76bbe35dcff081cde8ee185
Yifei Lu <yifeil0725@gmail.com>
2026-08-15 04:37:38 -0400

    import ops_logarithmic.py

--- python/needle/ops/ops_logarithmic.py
+++ python/needle/ops/ops_logarithmic.py
@@ -5,17 +5,31 @@ from ..autograd import TensorTuple, TensorTupleOp
 
 from .ops_mathematic import *
 
-from ..backend_selection import array_api, BACKEND 
+import numpy as array_api
 
 class LogSoftmax(TensorOp):
     def compute(self, Z: NDArray) -> NDArray:
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        Z_max = array_api.max(Z, axis=1, keepdims=True)
+        Z_stable = Z - Z_max
+        logsumexp = array_api.log(array_api.sum(array_api.exp(Z_stable), axis=1, keepdims=True))
+        return Z_stable - logsumexp
         ### END YOUR SOLUTION
 
     def gradient(self, out_grad: Tensor, node: Tensor):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        Z = node.inputs[0] # (N, C)
+        Z_data = Z.realize_cached_data() # (N, C)
+        Z_max = array_api.max(Z_data, axis=1, keepdims=True) # (N, 1)
+        Z_stable = Z_data - Z_max # (N, C)
+        logsumexp = array_api.log(array_api.sum(array_api.exp(Z_stable), axis=1, keepdims=True)) # (N, 1)
+        softmax = array_api.exp(Z_stable - logsumexp)   # (N, C)
+
+        sum_grad = summation(out_grad, axes=(1,)) # (1, C) -> (N,)
+
+        sum_grad = sum_grad.reshape((-1, 1)) # (N, 1)
+
+        return out_grad - multiply(sum_grad, Tensor(softmax)) # (N, C) - (N, 1) * (N, C)
         ### END YOUR SOLUTION
 
 
@@ -29,12 +43,26 @@ class LogSumExp(TensorOp):
 
     def compute(self, Z: NDArray) -> NDArray:
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        Z_max = array_api.max(Z, axis=self.axes, keepdims=True)
+        Z_stable = Z - Z_max
+        logsumexp = array_api.log(array_api.sum(array_api.exp(Z_stable), axis=self.axes, keepdims=True))
+        return (Z_max + logsumexp).squeeze()
         ### END YOUR SOLUTION
 
     def gradient(self, out_grad: Tensor, node: Tensor):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        Z = node.inputs[0].realize_cached_data() # (N, C)
+        max_Z = array_api.max(Z, axis=self.axes, keepdims=True) # (N, 1)
+        exp = array_api.exp(Z - array_api.broadcast_to(max_Z, Z.shape)) # (N, C)
+        sum_exp = array_api.sum(exp, axis=self.axes, keepdims=True) # (N, 1)
+        softmax = Tensor(exp / array_api.broadcast_to(sum_exp, exp.shape)) # (N, C)
+
+        if self.axes is not None:
+            shape = list(Z.shape)
+            for axis in self.axes:
+                shape[axis] = 1
+            out_grad = out_grad.reshape(shape)
+        return out_grad.broadcast_to(Z.shape) * softmax # (N, C)
         ### END YOUR SOLUTION
 
 
