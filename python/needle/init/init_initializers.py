3495b896366ba74964cb82925cf7b02b52f4f02b
Yifei Lu <yifeil0725@gmail.com>
2026-08-21 05:01:37 -0400

    kaiming_uniform

--- python/needle/init/init_initializers.py
+++ python/needle/init/init_initializers.py
@@ -5,30 +5,36 @@ from typing import Any
 
 def xavier_uniform(fan_in: int, fan_out: int, gain: float = 1.0, **kwargs: Any) -> "Tensor":
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    bound = gain * math.sqrt(6.0 / (fan_in + fan_out))
+    return rand(fan_in, fan_out, low = -bound, high = bound, **kwargs)
     ### END YOUR SOLUTION
 
 
 def xavier_normal(fan_in: int, fan_out: int, gain: float = 1.0, **kwargs: Any) -> "Tensor":
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    std = gain * math.sqrt(2.0 / (fan_in + fan_out))
+    return randn(fan_in, fan_out, mean = 0.0, std = std, **kwargs)
     ### END YOUR SOLUTION
 
-def kaiming_uniform(fan_in: int, fan_out: int, nonlinearity: str = "relu", **kwargs: Any) -> "Tensor":
+def kaiming_uniform(fan_in: int, fan_out: int, nonlinearity: str = "relu", **kwargs: Any, shape: tuple) -> "Tensor":
     assert nonlinearity == "relu", "Only relu supported currently"
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    if shape is not None:
+        fan_in = shape[0]
+        fan_out = shape[1]
+    if nonlinearity == "relu":
+        gain = math.sqrt(2.0)
+    bound = gain * math.sqrt(3.0 / fan_in)
+    return rand(fan_in, fan_out, low = -bound, high = bound, **kwargs)
     ### END YOUR SOLUTION
 
 
-def kaiming_uniform(fan_in, fan_out, shape=None, nonlinearity="relu", **kwargs):
-    assert nonlinearity == "relu", "Only relu supported currently"
-    ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
-    ### END YOUR SOLUTION
 
 def kaiming_normal(fan_in: int, fan_out: int, nonlinearity: str = "relu", **kwargs: Any) -> "Tensor":
     assert nonlinearity == "relu", "Only relu supported currently"
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    if nonlinearity == "relu":
+        gain = math.sqrt(2.0)
+    std = gain * math.sqrt(1.0 / fan_in)
+    return randn(fan_in, fan_out, mean = 0.0, std = std, **kwargs)
     ### END YOUR SOLUTION
